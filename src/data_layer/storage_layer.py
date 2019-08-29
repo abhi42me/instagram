3e138d4e12c8fc147bf977742b3c29a240175fdb
Abhishek <abhishek.arora@livspace.com>
2019-08-29 17:03:16 +0530

    code refactored

--- src/data_layer/storage_layer.py
+++ src/data_layer/storage_layer.py
@@ -1,11 +1,4 @@
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
+from util.singleton import Singleton
 
 class Storage(object):
     def __init__(self) -> None:
